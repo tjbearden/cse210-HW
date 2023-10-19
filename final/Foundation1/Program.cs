3ddc8e44915ac4554d7139ec71b3a94b7644003c
tjbearden <bea20021@byui.edu>
2023-10-18 19:42:04 -0600

    06

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        Video[] _videos = new Video[4];
+
+        _videos[0] = new Video("Blink-182", "YOU DON'T KNOW WHAT YOU'VE GOT", 198);
+        _videos[0].AddComment("oldmanmaggie", "I am a 38 year old father. Makes me proud to see my kids enjoying blink182 as much as I do");
+        _videos[0].AddComment("BLINKD In", "Best Band EVER");
+        _videos[0].AddComment("philbert123", "how have I not heard this song yet?");
+
+        _videos[1] = new Video("HeavenHelpMe", "How to have better Prayers", 603);
+        _videos[1].AddComment("MandyPants", "Great Advice, thanks for making this video");
+        _videos[1].AddComment("NotConvinced", "Get a life");
+        _videos[1].AddComment("Please Help me", "bruh");
+        
+        _videos[2] = new Video("purplesite", "How to get the best STLs for free", 750);
+        _videos[2].AddComment("Tim", "This is Fraud");
+        _videos[2].AddComment("CarlDB", "Don't click on the links, they is a scam");
+        _videos[2].AddComment("Bob'0'Head", "I got over 100 stls for free!.");
+
+        _videos[3] = new Video("Pam", "Workout to the Oldies", 6000);
+        _videos[3].AddComment("Jim Halpert", "Great Music, love the unitard on the dude.");
+        _videos[3].AddComment("Dwight Shrut", "I didn't even sweat");
+        _videos[3].AddComment("Kevin", "Exercise is not for me.");
+
+        for(int i = 0; i < _videos.Length ; i++)
+        {
+            Console.WriteLine();
+            _videos[i].DisplayVideoInfo();
+            _videos[i].DisplayVideoComments();
+        }
     }
 }
--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -1,9 +1,39 @@
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        Console.Clear();
+        Address[] addresses =  new Address[2];
+        Customer[] customers =  new Customer[2];
+        List<Product>[] productsLists = new List<Product>[2];
+        Order[] orders = new Order[2];
+
+        //ORDER 1 (USA)
+        addresses[0] = new Address("123 Main St", "Anytown", "Utah", "USA");
+        customers[0] =  new Customer("John Doe",addresses[0]);
+        productsLists[0] = new List<Product>();
+        productsLists[0].Add(new Product("Hersy Kisses 16oz","H142342343465",4.99f,1));
+        productsLists[0].Add(new Product("PB M&M 6oz","mars3kk34",2.50f,1));
+        orders[0] = new Order(customers[0],productsLists[0]);
+
+        //ORDER 2 (NOT USA)
+        addresses[1] = new Address("99456 High strasse", "West", "Hamburg", "Germany");
+        customers[1] =  new Customer("Fritz Dietz",addresses[1]);
+        productsLists[1] = new List<Product>();
+        productsLists[1].Add(new Product("Freeze Dried Taffy", "F1324564",12.99f,1));
+        productsLists[1].Add(new Product("Cocoa Beans","C45844",25.49f,2));
+        productsLists[1].Add(new Product("Confection Sugar","P11256",16.99f,2));
+        orders[1] = new Order(customers[1],productsLists[1]);
+
+        for(int i = 0; i < 2; i++)
+        {
+            Console.WriteLine($"\n-----CUSTOMER N°{i+1} ORDER-----");
+            Console.WriteLine($"PACKING LABEL:\n{orders[i].GetPackingLabel()}"); 
+            Console.WriteLine($"SHIPPING LABEL\n{orders[i].GetShippingLabel()}");
+            Console.WriteLine($"TOTAL COST: ${orders[i].GetTotalCost()}");
+        }
     }
 }
